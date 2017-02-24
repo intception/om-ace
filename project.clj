88ecb52c12b6dbf236409ced9a3bbb9a639bbcab
Sebastián Moreno <smoreno.uy@gmail.com>
2017-02-24 15:04:15 -0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.intception/om-ace "0.1.0"
+(defproject org.clojars.intception/om-ace "0.1.1"
   :description "An Om (ClojureScript) ace component"
   :url "http://github.com/intception/om-ace"
   :license {:name "Eclipse"
